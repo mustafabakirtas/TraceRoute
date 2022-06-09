88ff2225e303281d7c326f207b316e57f0824b25
mustafabakirtas <91268707+mustafabakirtas@users.noreply.github.com>
2022-06-09 12:58:58 +0300

    Add files via upload

---
+++ TraceRouteWindowsApp/TraceRouteWindowsApp - Kopya/TraceRouteWindowsApp/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace TraceRouteWindowsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(124, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(54, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP Adress";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 15;
+            this.listBox.Location = new System.Drawing.Point(54, 115);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(306, 229);
+            this.listBox.TabIndex = 5;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(285, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(285, 86);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSlateGray;
+            this.ClientSize = new System.Drawing.Size(446, 438);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Traceroute";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ TraceRouteWindowsApp/TraceRouteWindowsApp - Kopya/TraceRouteWindowsApp/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PcapDotNet.Core;
+using PcapDotNet.Packets;
+using PcapDotNet.Packets.Ethernet;
+using PcapDotNet.Packets.IpV4;
+using PcapDotNet.Core.Extensions;
+using PcapDotNet.Packets.Icmp;
+using System.Threading;
+using System.Diagnostics;
+
+
+namespace TraceRouteWindowsApp
+{
+    public partial class Form1 : Form
+    {
+        static Stopwatch sw = new Stopwatch();
+        static MacAddress sourceMAC;
+        static string sourceIP_str;
+        static LivePacketDevice selectedDevice;
+        static IpV4Address sourceIP;
+        static byte ttl_;
+        static IpV4Address destinationIP;
+        static int count = 50;
+        static List<IpV4Address> source_list = new List<IpV4Address>();
+        static List <string> pingvalue = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
+            sourceIP_str = null;
+            //****************************
+            selectedDevice = allDevices[7];
+            //*****************************
+            sourceMAC = selectedDevice.GetMacAddress();
+            foreach (DeviceAddress address in selectedDevice.Addresses)
+            {
+                if (address.Address.Family == SocketAddressFamily.Internet)
+                    sourceIP_str = address.Address.ToString().Substring(9, address.Address.ToString().Length - 9);
+            }
+            sourceIP = new IpV4Address(sourceIP_str);
+            
+        }
+        private static void icmp_send()
+        {
+            using (PacketCommunicator communicator = selectedDevice.Open(100, // name of the device
+                                                                         PacketDeviceOpenAttributes.Promiscuous, // promiscuous mode
+                                                                         1000)) // read timeout
+            {
+                
+                IcmpEchoLayer icmpLayer = new IcmpEchoLayer();
+                for (int i = 1; i < count; i++)
+                {
+                    Thread.Sleep(100);                    
+                    sw.Start();
+                    ttl_ = ((byte)i);
+                    communicator.SendPacket(BuildIcmpPacket());
+                    
+                }
+
+            }
+
+        }
+        public static void AddItemToListBox(ListBox listBox, string text)
+        {
+            // Check if the listbox need to be invoked.
+            if (listBox.InvokeRequired)
+                // Invoke the listbox control with the appropiate delegate.
+                listBox.Invoke(new Action<ListBox, string>(AddItemToListBox), listBox, text);
+            else
+            {
+                listBox.Items.Add(text);    
+                
+            }
+        }
+        private static Packet BuildIcmpPacket()
+        {
+            EthernetLayer ethernetLayer =
+                new EthernetLayer
+                {
+                    Source = sourceMAC,
+                    Destination = new MacAddress("8C:5A:C1:15:10:92"),
+                    EtherType = EthernetType.None, // Will be filled automatically.
+                };
+
+            IpV4Layer ipV4Layer =
+                new IpV4Layer
+                {
+                    Source = sourceIP,
+                    CurrentDestination = destinationIP,
+                    Fragmentation = IpV4Fragmentation.None,
+                    HeaderChecksum = null, // Will be filled automatically.
+                    Identification = 123,
+                    Options = IpV4Options.None,
+                    Protocol = null, // Will be filled automatically.
+                    Ttl = ttl_,
+                    TypeOfService = 0,
+                };
+
+            IcmpEchoLayer icmpLayer =
+                new IcmpEchoLayer
+                {
+                    Checksum = null, // Will be filled automatically.
+                    Identifier = 456,
+                    SequenceNumber = 800,
+                };
+
+            PacketBuilder builder = new PacketBuilder(ethernetLayer, ipV4Layer, icmpLayer);
+
+            return builder.Build(DateTime.Now);
+        }
+        private static void PacketHandler(Packet packet)
+        {
+            //Console.WriteLine(packet.Timestamp.ToString("yyyy-MM-dd hh:mm:ss.fff") + " length:" + packet.Length+"*******"+packet.IpV4.ToString());
+        }
+        void Dinle()
+        {
+            using (PacketCommunicator communicator =
+                selectedDevice.Open(65536,                                  
+                                                                            
+                                    PacketDeviceOpenAttributes.Promiscuous, 
+                                    1000))                                  
+            {
+
+                using (BerkeleyPacketFilter filter = communicator.CreateFilter("icmp && dst host " + "192.168.1.102"))
+                {
+                    
+                    communicator.SetFilter(filter);
+                }
+                string mesaj2;
+                Packet p;
+                do
+                {
+                    PacketCommunicatorReceiveResult result = communicator.ReceivePacket(out p); 
+                    switch (result)
+                    {
+                        case PacketCommunicatorReceiveResult.Timeout:
+                            if (ttl_ < count - 1)
+                                AddItemToListBox(listBox, "Time out");
+                            continue;
+                        case PacketCommunicatorReceiveResult.Ok: //id icmp layerdedir. 
+
+                            IpV4Datagram ip = p.Ethernet.IpV4;
+
+                            IpV4Address dst = ip.Destination, src = ip.Source;
+                            int x = (int)ip.Ttl;                            
+
+                            AddItemToListBox(listBox, $"Ip:{src}->TTL:{x}");
+                            //AddItemToListBox(listBox, x.ToString());
+                            Console.WriteLine("ttl status:" + x);
+                            source_list.Add(src);
+                            //source_list.Distinct().ToList();
+                            Console.WriteLine("Source:" + source_list.Last());
+                            sw.Stop();
+                            string elapsed1= sw.ElapsedMilliseconds.ToString();                        
+                            break;
+                        default:
+                            throw new InvalidOperationException("The result " + result + " should never be reached here");
+                    }
+                } while (source_list.Last().ToString() != destinationIP.ToString());//sonsuza kadar dinle.
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            source_list.Clear();
+            
+            //******************************************
+            Thread thread2 = new Thread(icmp_send);
+            Thread thread1 = new Thread(Dinle);
+            
+            try
+            {
+                destinationIP = new IpV4Address(textBox1.Text.ToString());
+                thread1.Start();
+                thread2.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            } 
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            listBox.Items.Clear();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
